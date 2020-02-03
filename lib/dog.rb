4b0df89e1ae5616f388a282465e7f42a6736f4c3
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-03 00:26:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,14 +5,12 @@ class Dog
     this_dogs_name = dog_name
   end
 
-  @this_dogs_name
-
   def name
     this_dogs_name
   end
 
-    lassie = Dog.new
-    lassie.name = "Lassie"
+    @lassie = Dog.new
+    @lassie.name = "Lassie"
 
 
 end
